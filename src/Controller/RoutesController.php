a6f6afe33e0f7aa23e89d70a5b35957c3ff19b41
Esteban <esteban.mansart@gmail.com>
2020-02-26 00:39:29 +0100

    [feat] CRD on Routes

--- src/Controller/RoutesController.php
+++ src/Controller/RoutesController.php
@@ -20,13 +20,20 @@ class RoutesController extends MainController {
      * @Router("/routes", name="routes_list", methods={"GET"})
      */
     public function read() {
+        $datas = [];
         $routeRepo = $this->getDoctrine()->getRepository(Route::class);
 
         $allRoutes = $routeRepo->findAll();
 
-        $datas["routes"] = $this->jms->serialize($allRoutes, "json");
+        $datas["routes"] = $allRoutes;
 
-        return new JsonResponse($datas);
+        $response = new JsonResponse(
+            $this->jms->serialize($datas, "json"),
+            200,
+            [],
+            true);
+
+        return $response;
     }
 
     /**
@@ -35,7 +42,6 @@ class RoutesController extends MainController {
      * @return Response
      */
     public function create(Request $request) {
-
         $returnResponse = [
             "status" => 500,
             "message" => "Unknown error",
@@ -63,6 +69,7 @@ class RoutesController extends MainController {
 
                     $returnResponse["status"] = 201;
                     $returnResponse["message"] = "Route créée avec succès";
+                    $returnResponse["datas"]["object_created"] = $entityFound;
 
                 } catch (\Exception $ex) {
                     $returnResponse["additional_message"] = $ex->getMessage();
@@ -75,7 +82,66 @@ class RoutesController extends MainController {
 
         }
 
-        return new JsonResponse(json_encode($returnResponse, JSON_UNESCAPED_UNICODE), $returnResponse["status"], [], true);
+        $response = new JsonResponse(
+            $this->jms->serialize($returnResponse, "json"),
+            $returnResponse["status"],
+            [],
+            true);
+
+        return $response;
+    }
+
+
+    /**
+     * @Router("/routes/{id<\d+>}", name="routes_delete", methods={"DELETE"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function delete(Request $request) {
+        $returnResponse = [
+            "status" => 500,
+            "message" => "Unknown error",
+            "additional_message" => "",
+            "datas" => []
+        ];
+        $em = $this->getDoctrine()->getManager();
+
+        $routeId = $request->attributes->get("id");
+        if (!empty($routeId)) {
+
+            $repoRoute = $this->getDoctrine()->getRepository(Route::class);
+            $routeToDelete = $repoRoute->find($routeId);
+
+            if (!empty($routeToDelete)) {
+
+                try {
+                    $em->remove($routeToDelete);
+                    $em->flush();
+                    $returnResponse["status"] = 200;
+                    $returnResponse["datas"]["object_deleted"] = [
+                        "id" => $routeId
+                    ];
+                    $returnResponse["message"] = "Route supprimée !";
+                } catch (\Exception $ex) {
+                    $returnResponse["message"] = "Impossible de supprimer la route";
+                    $returnResponse["additional_message"] = $ex->getMessage();
+                }
+
+            } else {
+
+                $returnResponse["status"] = 200;
+                $returnResponse["message"] = "Cette route n'existe pas ou plus.";
+
+            }
+        }
+
+        $response = new JsonResponse(
+            $this->jms->serialize($returnResponse, "json"),
+            $returnResponse["status"],
+            [],
+            true);
+
+        return $response;
     }
 
 }
